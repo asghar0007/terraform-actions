37366b437a05a2eb5ae321252baf489b61ece9a5
asghar0007 <60650810+asghar0007@users.noreply.github.com>
2023-04-22 03:55:15 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "git-action-demo"
 
     workspaces {
       name = "learn-terraform-github-actions"
